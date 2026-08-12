e15c2dbfab3e79302ef4e3727cb1908d0859e0a4
wickidcow <jaywago@gmail.com>
2026-08-12 15:47:43 -0400

    restore English Warfare guide categories

--- src/main/java/io/github/seggan/slimefunwarfare/lists/Categories.java
+++ src/main/java/io/github/seggan/slimefunwarfare/lists/Categories.java
@@ -6,51 +6,50 @@ import io.github.seggan.slimefunwarfare.SlimefunWarfare;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerHead;
-import org.bukkit.Material;
-
 import lombok.experimental.UtilityClass;
+import org.bukkit.Material;
 
 @UtilityClass
 public final class Categories {
 
     public static final ItemGroup GENERAL = new SubGroup(
         "slimefunwarfare_general",
-        new CustomItemStack(Material.DIAMOND_SWORD, "战争工艺 - 通用")
+        new CustomItemStack(Material.DIAMOND_SWORD, "Slimefun Warfare - General")
     );
 
     public static final ItemGroup MACHINES = new SubGroup(
         "slimefunwarfare_machines",
-        new CustomItemStack(Material.STONECUTTER, "战争工艺 - 机器")
+        new CustomItemStack(Material.STONECUTTER, "Slimefun Warfare - Machines")
     );
 
     public static final ItemGroup GUNS = new SubGroup(
         "slimefunwarfare_guns",
-        new CustomItemStack(Material.CROSSBOW, "战争工艺 - 枪械")
+        new CustomItemStack(Material.CROSSBOW, "Slimefun Warfare - Guns")
     );
 
     public static final ItemGroup MELEE = new SubGroup(
         "slimefunwarfare_melee",
-        new CustomItemStack(Material.IRON_AXE, "战争工艺 - 近战武器")
+        new CustomItemStack(Material.IRON_AXE, "Slimefun Warfare - Melee Weapons")
     );
 
     public static final ItemGroup EXPLOSIVES = new SubGroup(
         "slimefunwarfare_explosives",
-        new CustomItemStack(Material.TNT, "战争工艺 - 爆炸物")
+        new CustomItemStack(Material.TNT, "Slimefun Warfare - Explosives")
     );
 
     public static final ItemGroup RESOURCES = new SubGroup(
         "slimefunwarfare_resources",
-        new CustomItemStack(Material.IRON_ORE, "战争工艺 - 资源")
+        new CustomItemStack(Material.IRON_ORE, "Slimefun Warfare - Resources")
     );
 
     public static final ItemGroup POWER_SUITS = new SubGroup(
         "slimefunwarfare_power_suits",
-        new CustomItemStack(PlayerHead.getItemStack(Heads.SUIT_HELMET), "战争工艺 - 动力装甲")
+        new CustomItemStack(PlayerHead.getItemStack(Heads.SUIT_HELMET), "Slimefun Warfare - Power Suits")
     );
 
     private static final ItemGroup MAIN = new MultiGroup(
         "slimefunwarfare",
-        new CustomItemStack(Material.DIAMOND_SWORD, "战争工艺"),
+        new CustomItemStack(Material.DIAMOND_SWORD, "Slimefun Warfare"),
         GENERAL, MACHINES, GUNS, MELEE, EXPLOSIVES, RESOURCES, POWER_SUITS
     );
 
